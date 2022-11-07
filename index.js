0480222af04c3d479257b488bb21c55d8e73d15f
atikdev-bd <atikdev.bd@gmail.com>
2022-11-08 01:40:53 +0600

    service data get to mongodv

--- index.js
+++ index.js
@@ -1,42 +1,45 @@
-const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const cors = require('cors');
-const app = express()
-const port = process.env.PORT || 5000
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
 require("dotenv").config();
 
 // middle ware \\\
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 //Mongodb implement here \\\
 
-
 const uri = `mongodb+srv://${process.env.BROD_BRAND}:${process.env.SECRET_KEY}@cluster0.h7epoo8.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-const run = async() =>{
+const run = async () => {
+  try {
+    const serviceCollection = client.db("brodBrand").collection("services");
 
-    try{
+    /// read a all data with use empty objects and find () ///
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+};
 
+run().catch((error) => console.log(error));
 
-        
-    }
-   finally{
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-   }
-
-}
-
-run().catch(error =>console.log(error))
-
-
-app.get('/', (req, res)=>{
-    res.send('Server is running')
-})
-
-
-app.listen(port, ()=>{
-    console.log('server is running on port :', port)
-})
+app.listen(port, () => {
+  console.log("server is running on port :", port);
+});
