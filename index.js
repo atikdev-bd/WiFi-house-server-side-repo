49b1f41efb184ac251cc81600fa0ecfc2d178044
atikdev-bd <atikdev.bd@gmail.com>
2022-11-09 01:56:36 +0600

    create jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
@@ -23,12 +24,36 @@ const run = async () => {
   try {
     const serviceCollection = client.db("brodBrand").collection("services");
 
-    /// read a all data with use empty objects and find () ///
+    ///explore jwt token ///
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+      res.send({ token });
+    });
+
+    /// read a 3 data use limit///
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
       const services = await cursor.limit(3).toArray();
       res.send(services);
+    });
+    /// read a all service with use query ///
+
+    app.get("/allService", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const allService = await cursor.toArray();
+      res.send(allService);
+    });
+    //get data with id///
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      console.log(service);
+      res.send(service);
     });
   } finally {
   }
